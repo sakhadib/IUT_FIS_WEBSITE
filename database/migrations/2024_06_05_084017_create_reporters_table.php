4d748ba0da8cefc140729a1b50d0ef05351ff2b5
sakhadib <adibsakhawat@iut-dhaka.edu>
2024-06-05 14:44:04 +0600

    feat: Add Reporter model and migration
    The commit adds a new file for the Reporter model and defines the table name, primary key, and fillable fields for the model. The HasFactory trait is also used for the model. Additionally, a migration file is created for creating the reporters table.
    
    #changelog
    - Added Reporter model and migration

---
+++ database/migrations/2024_06_05_084017_create_reporters_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('reporters', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('member_id');
+            $table->string('password');
+            $table->boolean('is_pass_changed')->default(false);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('reporters');
+    }
+};
